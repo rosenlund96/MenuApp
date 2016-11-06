5d71657ad5205d64502a276f4fef2709b18589a6
rosenlund96 <Stumack3r>
2016-11-07 00:45:01 +0100

    ...

---
+++ MenuApp/app/src/main/java/com/galgespil/stvhendeop/menuapp/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.galgespil.stvhendeop.menuapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+
+
+public class LoginActivity extends AppCompatActivity{
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
