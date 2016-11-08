e16156fb3f989cc14a8c89ddecb8608dcaab407a
rosenlund96 <Stumack3r>
2016-11-08 18:08:16 +0100

    ...

---
+++ MenuApp/app/src/main/java/com/galgespil/stvhendeop/menuapp/PrefFragment.java
@@ -0,0 +1,23 @@
+package com.galgespil.stvhendeop.menuapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mathiaslarsen on 07/11/2016.
+ */
+public class PrefFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_pref, container, false);
+
+
+    }
+    }
