b2d7d50f31609c9778311803e62a1cb46cf65305
rosenlund96 <Stumack3r>
2016-11-21 11:34:53 +0100

    Nyt nyt nyt

---
+++ MenuApp/app/src/main/java/com/grp12/softskilltools/resources/ItemDefinition.java
@@ -0,0 +1,31 @@
+package com.grp12.softskilltools.resources;
+
+import com.grp12.softskilltools.Entities.AbstractItem;
+
+/**
+ * Created by mathiaslarsen on 21/11/2016.
+ */
+
+public class ItemDefinition {
+
+    public final static int [] TESTNo_DATA = {
+            1,2,3
+    };
+    public static final String [] TESTName_DATA = {
+            "DISC",
+            "Belbin",
+            "ThreeSixty"
+};
+public static final int [] TESTPrice_DATA = {
+        30,50,80
+        };
+
+
+public static final AbstractItem.testType testType[]  = {
+        AbstractItem.testType.DISC,
+        AbstractItem.testType.BELBIN,
+        AbstractItem.testType.THREESIXTY
+        };
+
+
+        }
